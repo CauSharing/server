5696b525e213266bf9d88da6827ce13677e60212
wlrhkd49 <wlrhkd49@naver.com>
2021-10-02 19:11:23 +0900

    first

---
+++ src/main/java/causharing/causharing/CausharingApplication.java
@@ -0,0 +1,13 @@
+package causharing.causharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CausharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CausharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/causharing/causharing/CausharingApplicationTests.java
@@ -0,0 +1,13 @@
+package causharing.causharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CausharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
