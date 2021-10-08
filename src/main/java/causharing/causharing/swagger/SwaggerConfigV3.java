09030ed56ba065d6b1a8994025855dbae3f9395a
wlrhkd49 <wlrhkd49@naver.com>
2021-10-08 12:46:57 +0900

    add Spring Security in swagger

--- src/main/java/causharing/causharing/swagger/SwaggerConfigV3.java
+++ src/main/java/causharing/causharing/swagger/SwaggerConfigV3.java
@@ -1,19 +1,18 @@
-package com.mr.myrecord.swagger;
+package causharing.causharing.swagger;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiKey;
 import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import java.util.*;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
