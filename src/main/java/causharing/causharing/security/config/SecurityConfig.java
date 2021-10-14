bcc098d7e09f409f6fb44093f9ad4031b5538f6f
wlrhkd49 <wlrhkd49@naver.com>
2021-10-14 12:27:41 +0900

    re modify

--- src/main/java/causharing/causharing/security/config/SecurityConfig.java
+++ src/main/java/causharing/causharing/security/config/SecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableWebSecurity
-@CrossOrigin(origins="*", maxAge=3600)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
